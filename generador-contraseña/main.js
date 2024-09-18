6d577cdd87c3fa8aee155355552702a4c9b5cdb6
Ruth C <ruthcusihuaman@gmail.com>
2024-09-17 21:38:11 -0500

    modificacion del archivo JS v2

--- generador-contraseña/main.js
+++ generador-contraseña/main.js
@@ -1,4 +1,12 @@
+//declaracion de contraseñas
+var cantidad=document.getElementById('cantidad');
 
+let texto="texto";
+console.log(typeof texto);
+
+if(cantidad <6){
+    alert("La cantidad de caracteres tiene que ser mayor que 6");
+}
 
 
 
